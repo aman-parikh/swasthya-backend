05ad9dae5d4d26631de6b357939ad196ad768a60
aman-parikh <amanparikh14@gmail.com>
2022-03-17 13:24:05 +0530

    allow app to use json data

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 require('dotenv').config();
 require('./config/database')
 let portNumber = process.env.PORT || 3600
